fd9c902098d0fdc370d732ca733e2b4e6b14bc52
Amal-nellanhi <amalnellanhi@gmail.com>
2026-03-14 12:22:55 +0530

    Add Rock Paper Scissors project description

---
+++ rock_paper_scissors.py
@@ -0,0 +1,23 @@
+"""
+Project: Rock Paper Scissors Game
+
+Description:
+Create a Rock-Paper-Scissors game where the user plays against the computer.
+
+Requirements:
+1. Ask the user to choose:
+   R for Rock
+   P for Paper
+   S for Scissors
+2. The computer should randomly select its choice.
+3. Display both choices using emojis if possible.
+4. Determine the winner based on the rules.
+5. Allow the user to play multiple rounds until they exit.
+
+Sample Output:
+
+Choose (R/P/S): R
+You chose: Rock 🪨
+Computer chose: Scissors ✂️
+You win!
+"""
