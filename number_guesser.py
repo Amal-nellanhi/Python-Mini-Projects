0fb08527aa543f5bc76219f3fa5f51a5bc442b2b
NivedithaChandran <nivedithachandran45@gmail.com>
2026-03-14 21:37:19 +0530

    Added author name and finalize program.Closes#<4>

--- number_guesser.py
+++ number_guesser.py
@@ -1,5 +1,6 @@
 """
 Project: Number Guessing Game
+Author:Niveditha T Chandran
 
 Description:
 Create a game where the user tries to guess a randomly generated number.
@@ -26,4 +27,12 @@ Congratulations! You guessed the number.
 import random
 number=random.randint(1,100)
 print("Guess the number between 1 and 100")
+while True:
 guess=int(input("Enter your guess:"))
+     if guess>number:
+        print("Too high")
+     elif guess<number:
+        print("Too low")
+     else:
+        print("Congratulations! You guessed the number.")
+    break
