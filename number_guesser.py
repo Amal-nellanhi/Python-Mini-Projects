1cf052db3ff144c353aaf24575dc2ba84bf7e660
Amal-nellanhi <amalnellanhi@gmail.com>
2026-03-14 12:21:58 +0530

    Add Number Guesser project description

---
+++ number_guesser.py
@@ -0,0 +1,25 @@
+"""
+Project: Number Guessing Game
+
+Description:
+Create a game where the user tries to guess a randomly generated number.
+
+Requirements:
+1. Generate a random number between 1 and 100.
+2. Ask the user to guess the number.
+3. If the guess is too high, display "Too high".
+4. If the guess is too low, display "Too low".
+5. Continue until the user guesses the correct number.
+
+Sample Output:
+
+Guess the number between 1 and 100
+Enter your guess: 50
+Too high
+
+Enter your guess: 20
+Too low
+
+Enter your guess: 35
+Congratulations! You guessed the number.
+"""
