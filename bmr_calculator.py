25cbd9de0b16a1cff3fa71115a7cbf8fcec3d23b
Meenakshi-Dasan <meenakshidasan149@gmail.com>
2026-03-14 23:52:36 -0800

    bmr calculated

--- bmr_calculator.py
+++ bmr_calculator.py
@@ -29,3 +29,25 @@ Weight: 70
 Sample Output:
 Your BMR is: 1673 calories/day
 """
+gender = input("Enter Gender (Male/Female): ")
+age = int(input("Enter Age: "))
+height = float(input("Enter Height (cm): "))
+weight = float(input("Enter Weight (kg): "))
+
+if gender.lower() == "male":
+    bmr = 10 * weight + 6.25 * height - 5 * age + 5
+elif gender.lower() == "female":
+    bmr = 10 * weight + 6.25 * height - 5 * age - 161
+else:
+    print("Invalid gender entered")
+    exit()
+
+print("Your BMR is:", int(bmr), "calories/day")
+
+
+
+
+
+
+
+
