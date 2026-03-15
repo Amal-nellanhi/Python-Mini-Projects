529a814807a6a09e655eac2131606cb42b1ba47c
Amal-nellanhi <amalnellanhi@gmail.com>
2026-03-15 09:55:42 +0530

    Fix indentation errors and improve code formatting Co-authored-by: NivedithaChandran <nivedithachandran45@gmail.com>

--- number_guesser.py
+++ number_guesser.py
@@ -28,11 +28,11 @@ import random
 number=random.randint(1,100)
 print("Guess the number between 1 and 100")
 while True:
-guess=int(input("Enter your guess:"))
-     if guess>number:
-        print("Too high")
-     elif guess<number:
-        print("Too low")
-     else:
-        print("Congratulations! You guessed the number.")
-    break
+   guess=int(input("Enter your guess:"))
+   if guess>number:
+      print("Too high")
+   elif guess<number:
+     print("Too low")
+   else:
+     print("Congratulations! You guessed the number.")
+     break
